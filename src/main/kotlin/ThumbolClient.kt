fcd21634ac6e2b14788a37d643af9b6b5487c02e
MaxPC_NECTEC <singmax161@gmail.com>
2017-08-07 16:55:46 +0700

    Init Value

--- src/main/kotlin/ThumbolClient.kt
+++ src/main/kotlin/ThumbolClient.kt
@@ -1,6 +1,16 @@
 class ThumbolClient {
-    fun getImage(part_file: String){
 
+    //http://localhost:8000/unsafe/trim/fit-in/500x500/filters:blur(7):brightness(40)/1.jpg
+    //http://localhost:8000/unsafe/trim/fit-in/500x500/filters:blur(7):brightness(-40)/1.jpg
+
+    val default_width = 500
+    val default_height = 500
+
+    val trim="Enable"
+    val fitin="Disable"
+
+
+    fun getImage(part_file: String){
     }
     fun getImage(filter: String,part_file: String){
 
