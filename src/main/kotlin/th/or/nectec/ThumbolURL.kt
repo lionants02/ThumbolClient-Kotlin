b0abec36d443660ccf29d9f7592714629c065932
Max Thanachai <singmax161@gmail.com>
2017-08-10 02:18:14 +0700

    As Home

--- src/main/kotlin/th/or/nectec/ThumbolURL.kt
+++ src/main/kotlin/th/or/nectec/ThumbolURL.kt
@@ -1,92 +1,58 @@
 package th.or.nectec
 
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.getAs
-import com.sun.org.apache.xpath.internal.operations.Bool
-
 /**
  * How To Parameter
  * https://thumbor.readthedocs.io/en/latest/filters.html
  */
 class ThumbolURL(// = "http://localhost:8000"
-        var hosturl: String, //= "1.jpg"
+        var hosturl: String,
         var imagename: String) {
 
     //http://localhost:8000/unsafe/trim/fit-in/500x500/filters:blur(7,0):brightness(10)/1.jpg
 
     var hmac_keyAUTH: String? = null
-
     var background_color: String? = null
-
     var blur: Int = 0
     var blur_sigma: Int = 0
-
     var brightness: Int = 0
     var contrast: Int = 0
-
     var equalize: Boolean = false
-
     var format: String? = null
-
     var grayscale: Boolean = false
-
-
     var rAmount: Int = 0
     var gAmount: Int = 0
     var bAmount: Int = 0
-
     var rotate: Int = 0
-
     var strip_icc: Boolean = false
-
-
     var max_bytes: Long? = null
-
     var no_upscale: Boolean = false
-
     var noise: Int = 0
     var quality: Int? = null
-    var red_eye: Boolean = false
 
     fun getUrl(): String {
         var genurl = hosturl
         genurl += hmac_keyAUTHFilter()
         genurl += addFilter()
         genurl += addFilename()
-
-
         return genurl
     }
 
     private fun addFilter(): String {
         var filter = "/filters"
-        filter += background_colorFilter()
-
+        filter += backgroundColorFilter()
         filter += blurFilter()
-
         filter += brightnessFilter()
         filter += contrastFilter()
-
         filter += equalizeFilter()
-
         filter += formatFilter()
-
         filter += grayscaleFilter()
-
-        filter += max_bytesFilter()
-
-        filter += no_upscaleFilter()
-
+        filter += maxBytesFilter()
+        filter += noUpscaleFilter()
         filter += noiseFilter()
         filter += qualityFilter()
-        filter += red_eyeFilter()
-
         filter += rgbAmountFilter()
-
         filter += rotateFilter()
-
-        filter += strip_iccFilter()
+        filter += stripIccFilter()
         return if (!filter.endsWith("/filters")) filter else ""
     }
 
@@ -109,7 +75,7 @@ class ThumbolURL(// = "http://localhost:8000"
     }
 
 
-    private fun background_colorFilter(): String {
+    private fun backgroundColorFilter(): String {
         return if (background_color != null) {
             ":background_color(" + background_color.toString() + ")"
         } else ""
@@ -118,20 +84,20 @@ class ThumbolURL(// = "http://localhost:8000"
 
     private fun blurFilter(): String {
         return if (blur != 0) {
-            ":blur(" + blur.toString() + "," + blur_sigma.toString() + ")"
+            ":blur(" + blur.toString() + "," + blur_sigma + ")"
         } else ""
     }
 
 
     private fun brightnessFilter(): String {
         return if (brightness != 0) {
-            ":brightness(" + brightness.toString() + ")"
+            ":brightness(" + brightness + ")"
         } else ""
     }
 
     private fun contrastFilter(): String {
         return if (contrast != 0) {
-            ":contrast(" + contrast.toString() + ")"
+            ":contrast(" + contrast + ")"
         } else ""
     }
 
@@ -144,7 +110,7 @@ class ThumbolURL(// = "http://localhost:8000"
 
     private fun formatFilter(): String {
         return if (format != null) {
-            ":format(" + format.toString() + ")"
+            ":format(" + format + ")"
         } else ""
     }
 
@@ -156,58 +122,55 @@ class ThumbolURL(// = "http://localhost:8000"
 
     private fun rgbAmountFilter(): String {
         return if ((rAmount or gAmount or bAmount) != 0) {
-            ":rgb(" + rAmount.toString() + "," + gAmount.toString() + "," + bAmount.toString() + ")"
+            ":rgb(" + rAmount + "," + gAmount + "," + bAmount + ")"
         } else ""
     }
 
 
     private fun rotateFilter(): String {
         return if (rotate != 0) {
-            ":rotate(" + rotate.toString() + ")"
+            ":rotate(" + rotate + ")"
         } else ""
     }
 
     /////////////////////////////////---------------
-    private fun max_bytesFilter(): String {
+
+    private fun maxBytesFilter(): String {
         return if (max_bytes != null) {
-            "/" + max_bytes.toString()
+            ":max_bytes(" + max_bytes + ")"
         } else ""
     }
 
-
-    private fun no_upscaleFilter(): String {
+    private fun noUpscaleFilter(): String {
         return if (no_upscale) {
-            "/" + no_upscale.toString()
+            ":no_upscale()"
         } else ""
     }
 
 
     private fun noiseFilter(): String {
         return if (noise != 0) {
-            "/" + noise.toString()
+            ":noise(" + noise + ")"
         } else ""
     }
+
 
     private fun qualityFilter(): String {
         return if (quality != null) {
-            "/" + quality.toString()
-        } else ""
-    }
-
-    private fun red_eyeFilter(): String {
-        return if (red_eye) {
-            "/" + red_eye.toString()
+            ":quality(" + quality + ")"
         } else ""
     }
 
 
-    private fun strip_iccFilter(): String {
+
+
+    private fun stripIccFilter(): String {
         return if (strip_icc) {
-            "/" + strip_icc.toString()
+            ":strip_icc()"
         } else ""
     }
 
-
+    //----------
     private fun addFilename(): String {
         return "/" + imagename
     }
--- src/test/kotlin/th/or/nectec/ThumbolClientTest.kt
+++ src/test/kotlin/th/or/nectec/ThumbolClientTest.kt
@@ -5,39 +5,50 @@ import org.junit.Assert
 import org.junit.Test
 
 class ThumbolClientTest {
-    val clientTest1 : ThumbolURL = ThumbolURL("http://localhost:8000","1.jpg").apply { brightness=100
+    val clientTest1: ThumbolURL = ThumbolURL("http://localhost:8000", "1.jpg").apply {
+        brightness = 100
     }
 
 
     val timeout = 5000 // 5000 milliseconds = 5 seconds.
     val readTimeout = 9000 // 9000 milliseconds = 9 seconds.
 
-    val testurl :String ="http://localhost:8000/unsafe/trim/fit-in/500x500/filters:blur(7):brightness(40)/1.jpg"
+    val testurl: String = "http://localhost:8000/unsafe/trim/fit-in/500x500/filters:blur(7):brightness(40)/1.jpg"
 
-    @Test fun getUrl1(){
+    @Test fun getUrl1() {
 
-        var thumbolurlTest :ThumbolURL = ThumbolURL("http://localhost:8000","1.jpg").apply { blur=10 }.apply { blur_sigma=30 }.apply { brightness=10 }.apply { contrast=50 }.apply { equalize=true }.apply { format="png" }.apply {  grayscale =false}
-        //System.out.println(thumbolurlTest.getUrl())
+        var thumbolurlTest: ThumbolURL = ThumbolURL("http://localhost:8000", "1.jpg").
+                apply {
+                    blur = 10
+                    blur_sigma = 30
+                    brightness = 10
+                    contrast = 50
+                    equalize = true
+                    format = "png"
+                    grayscale = false
+                }
+        Assert.assertEquals("http://localhost:8000/unsafe/filters:blur(10,30):brightness(10):contrast(50):equalize():format(png)/1.jpg", thumbolurlTest.getUrl())
 
-        Assert.assertEquals("http://localhost:8000/unsafe/filters:blur(10,30):brightness(10):contrast(50):equalize():format(png)/1.jpg",thumbolurlTest.getUrl() )
-        thumbolurlTest=ThumbolURL("http://localhost:8000","1.jpg").apply {
-            rAmount=20
+        thumbolurlTest = ThumbolURL("http://localhost:8000", "1.jpg").apply {
+            rAmount = 20
+            gAmount = -20
+            bAmount = 40
             rotate = 180
-        }.apply { gAmount=-20 }.apply { bAmount=40 }
+        }
         System.out.println(thumbolurlTest.getUrl())
     }
 
 
-    @Test fun testDownload(){
+    @Test fun testDownload() {
 
-        var i =0
+        var i = 0
 
-        var data:ByteArray
+        var data: ByteArray
         testurl.httpGet().response { request, response, result ->
 
-            if (response.httpStatusCode==200){
+            if (response.httpStatusCode == 200) {
 
-                data=result.get()
+                data = result.get()
             }
             System.out.println(result);
             System.out.println("ssss");
@@ -46,9 +57,8 @@ class ThumbolClientTest {
         }
         //System.out.println("ddddd");
 
-        while (i<1)Thread.sleep(2000)
+        while (i < 1) Thread.sleep(2000)
     }
-
 
 
 }
